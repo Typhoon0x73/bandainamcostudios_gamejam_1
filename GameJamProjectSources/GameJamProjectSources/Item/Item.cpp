7cc967abe02b9cfbdff80429ea3455f5da9484bb
Typhoon0x73 <akasugitasuisei@gmail.com>
2023-10-26 08:57:39 +0900

    ver1.0.0 アイテムの準備

---
+++ GameJamProjectSources/GameJamProjectSources/Item/Item.cpp
@@ -0,0 +1,39 @@
+﻿#include "Item.h"
+
+namespace bnscup
+{
+
+	Item::Item()
+		: m_pos{ Vec2::Zero() }
+		, m_texture{}
+		, m_srcRect{ RectF::Empty() }
+	{
+	}
+
+	Item::~Item()
+	{
+	}
+
+	void Item::draw() const
+	{
+		Vec2 pos = m_pos;
+		pos.y + m_srcRect.h * 0.5;
+		m_texture(m_srcRect).drawAt(pos);
+	}
+
+	void Item::setPos(const Vec2& pos)
+	{
+		m_pos = pos;
+	}
+
+	void Item::setTexture(AssetNameView name)
+	{
+		m_texture = TextureAsset(name);
+	}
+
+	void Item::setSrcRect(const RectF& srcRect)
+	{
+		m_srcRect = srcRect;
+	}
+
+}
---
+++ GameJamProjectSources/GameJamProjectSources/Item/Item.h
@@ -0,0 +1,30 @@
+﻿#pragma once
+#ifndef BNSCUP_ITEM_H_
+#define BNSCUP_ITEM_H_
+
+#include <Siv3D.hpp>
+
+namespace bnscup
+{
+	class Item
+	{
+	public:
+
+		explicit Item();
+		virtual ~Item();
+
+		void draw() const;
+
+		void setPos(const Vec2& pos);
+		void setTexture(AssetNameView name);
+		void setSrcRect(const RectF& srcRect);
+
+	private:
+
+		Vec2 m_pos;
+		Texture m_texture;
+		RectF m_srcRect;
+	};
+}
+
+#endif // !BNSCUP_ITEM_H_
